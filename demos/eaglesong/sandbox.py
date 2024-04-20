a442ed4ea6cf78c046c17cfb4e7ace7bafcad59f
dkamenshchik <dkamenshchik@gmail.com>
2024-04-20 19:35:08 +0300

    Add streak tracker skill

--- demos/eaglesong/sandbox.py
+++ demos/eaglesong/sandbox.py
@@ -1,15 +1,19 @@
 from demos.eaglesong.common import *
 from demos.eaglesong.example_06_auth_4 import Authorize
-from demos.eaglesong.example_08_menu_2 import create_menu
+from kaia.eaglesong.amenities import menu
+from kaia.eaglesong.amenities.menu import ValueMenuItem, FunctionalMenuItem, MenuFolder
 from zoo.assistant.skills.tasks.reminder import ReminderList
+from zoo.assistant.skills.tasks.streak_tracker import StreakTracker, Task
 
 
 def main():
     reminder = ReminderList.with_presets()
+    streak_tracker = StreakTracker([])
+    streak_tracker.load_from_file()
     context = yield ContextRequest()
     timer_state = False
     menu_aut = None  # type: Optional[Automaton]
-    yield 'This bot will send you reminders. Enter /toggle to pause/resume, or /menu to open menu.'
+    yield 'This bot will send you reminders. Enter /toggle to pause/resume, or /streak to open streak menu.'
     input = yield Listen()
     while True:
         if isinstance(input, TimerTick):
@@ -25,8 +29,8 @@ def main():
             input = yield Listen()
             continue
 
-        if input == '/menu':
-            menu_aut = Automaton(create_menu(), context)
+        if input == '/streak':
+            menu_aut = Automaton(create_menu(streak_tracker), context)
 
         if menu_aut is not None:
             result = menu_aut.process(input)
@@ -39,6 +43,22 @@ def main():
 
         yield f'Unexpected input: {input}'
         input = yield Listen()
+
+
+def update_streak(task, streak_tracker: StreakTracker):
+    new_streak = streak_tracker.recalculate_streak(task.id)
+    streak_tracker.save_to_file()
+    yield f'{task.description} streak is {new_streak}'
+
+
+def create_menu(streak_tracker: StreakTracker):
+    menu_items = [FunctionalMenuItem(
+        f'{task.description}: {task.streak}',
+        (lambda task_local=task: update_streak(task_local, streak_tracker)),
+        True)
+        for task in streak_tracker.get_tasks()]
+
+    return MenuFolder('Streak').items(*menu_items)
 
 
 bot = Bot("timer2", Authorize('KAIA_TEST_BOT_CHAT_ID', main), timer=True, timer_interval=60)
---
+++ zoo/assistant/skills/tasks/streak_tracker.py
@@ -0,0 +1,64 @@
+import csv
+import os
+from typing import List
+import datetime
+import json
+
+
+class Task:
+    def __init__(self, id: int, description: str, dates: List[datetime.date]):
+        self.id = id
+        self.description = description
+        self.dates = list(set(dates))
+        self.streak = self.calculate_streak()
+
+    def calculate_streak(self):
+        self.dates.sort()
+        if len(self.dates) <= 1:
+            return len(self.dates)
+        streak = 1
+        for date1, date2 in zip(self.dates, self.dates[1:]):
+            if date2 - date1 == datetime.timedelta(days=1):
+                streak += 1
+            else:
+                streak = 1
+        return streak
+
+    def add_date(self, date: datetime.date):
+        new_dates = self.dates + [date]
+        return Task(self.id, self.description, new_dates)
+
+
+class StreakTracker:
+    def __init__(self, tasks: List[Task]):
+        self.tasks = {task.id: task for task in tasks}
+
+    def get_tasks(self) -> [Task]:
+        return list(self.tasks.values())
+
+    def recalculate_streak(self, task_id: int):
+        if task_id in self.tasks:
+            new_task = self.tasks[task_id].add_date(datetime.date.today())
+            self.tasks[task_id] = new_task
+            return new_task.streak
+
+    def save_to_file(self, filename: str = None):
+        filename = filename or os.environ.get('STREAK_TRACKER_PATH')
+        with open(filename, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(['id', 'description', 'dates'])
+            for task in self.get_tasks():
+                writer.writerow([task.id, task.description, ';'.join(map(str, task.dates))])
+
+    def load_from_file(self, filename: str = None):
+        filename = filename or os.environ.get('STREAK_TRACKER_PATH')
+        with open(filename, 'r') as f:
+            reader = csv.reader(f)
+            next(reader)
+            self.tasks = {
+                int(row[0]): Task(
+                    int(row[0]),
+                    row[1],
+                    [] if not row[2] else list(map(datetime.date.fromisoformat, filter(None, row[2].split(';'))))
+                )
+                for row in reader}
